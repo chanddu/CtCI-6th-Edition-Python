3b7e2adc7b69468fbb9c2b2399d6ba871ee0f9f0
Chandra Sekhar Guntupalli <chanddu@users.noreply.github.com>
2017-05-23 18:32:38 -0500

    Update OneAway.py

--- Chapter1/5_One Away/OneAway.py
+++ Chapter1/5_One Away/OneAway.py
@@ -2,41 +2,18 @@
 import unittest
 
 
-def one_away(s1, s2):
+def one_away(a, b):
     '''Check if a string can converted to another string with a single edit'''
-    if len(s1) == len(s2):
-        return one_edit_replace(s1, s2)
-    elif len(s1) + 1 == len(s2):
-        return one_edit_insert(s1, s2)
-    elif len(s1) - 1 == len(s2):
-        return one_edit_insert(s2, s1)
-    return False
-
-
-def one_edit_replace(s1, s2):
-    edited = False
-    for c1, c2 in zip(s1, s2):
-        if c1 != c2:
-            if edited:
-                return False
-            edited = True
-    return True
-
-
-def one_edit_insert(s1, s2):
-    edited = False
-    i, j = 0, 0
-    while i < len(s1) and j < len(s2):
-        if s1[i] != s2[j]:
-            if edited:
-                return False
-            edited = True
-            j += 1
-        else:
-            i += 1
-            j += 1
-    return True
-
+    a = list(a.strip())
+    b = list(b.strip())
+    length_intersection = 0
+    for e in a:
+        if e in b:
+            length_intersection+=1
+    if length_intersection < (max(len(a),len(b)) - 1):
+        return False
+    else:
+        return True
 
 class Test(unittest.TestCase):
     '''Test Cases'''
